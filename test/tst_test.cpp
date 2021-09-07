af54ff5943a78bb86267436f95ef5902767451ce
Artemvv88 <artemvv@list.ru>
2021-09-08 01:29:14 +0700

    Add tasks tests

--- test/tst_test.cpp
+++ test/tst_test.cpp
@@ -16,6 +16,10 @@ private slots:
     void test_modify_user();
     void test_remove_user();
 
+    void test_add_task();
+    void test_modify_task();
+    void test_remove_task();
+
 };
 
 Test::Test()
@@ -51,6 +55,29 @@ void Test::test_remove_user()
 {
     Database::instance()->removeUserById(Database::instance()->getUserByLogin("test")->id());
     Q_ASSERT(Database::instance()->getUserByLogin("test") == nullptr);
+}
+
+void Test::test_add_task()
+{
+    auto newTask = new Task();
+    newTask->setDescription("test");
+    newTask->setExecutorGroup(SystemUserType::WORKER);
+    Database::instance()->saveTask(newTask);
+    Q_ASSERT(Database::instance()->getTaskById(newTask->id())->id() == newTask->id());
+}
+
+void Test::test_modify_task()
+{
+    auto task = Database::instance()->tasks().last();
+    task->setDone(true);
+    Q_ASSERT(Database::instance()->getTaskById(task->id())->done() == true);
+}
+
+void Test::test_remove_task()
+{
+    auto taskId = Database::instance()->tasks().last()->id();
+    Database::instance()->removeTaskById(taskId);
+    Q_ASSERT(Database::instance()->tasks().last()->id() != taskId);
 }
 
 QTEST_APPLESS_MAIN(Test)
