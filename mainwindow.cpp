c44b07ceabfdf181335416f904ac39b1d547995b
Artemvv88 <artemvv@list.ru>
2021-09-07 16:58:09 +0700

    Created ui and stub functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,7 +13,54 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    auto w = new LoginWindow();
+    w->show();
+    event->accept();
+}
+
 void MainWindow::setUser(SystemUser *user)
 {
     _user = user;
+}
+
+void MainWindow::on_addUserBtn_clicked()
+{
+
+}
+
+void MainWindow::on_addRackBtn_clicked()
+{
+
+}
+
+void MainWindow::on_addProductBtn_clicked()
+{
+
+}
+
+void MainWindow::on_addTask_clicked()
+{
+
+}
+
+void MainWindow::on_usersTable_doubleClicked(const QModelIndex &index)
+{
+
+}
+
+void MainWindow::on_racksTable_doubleClicked(const QModelIndex &index)
+{
+
+}
+
+void MainWindow::on_productsTable_doubleClicked(const QModelIndex &index)
+{
+
+}
+
+void MainWindow::on_tasksTable_doubleClicked(const QModelIndex &index)
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QCloseEvent>
 
+#include "database.h"
+#include "loginwindow.h"
 #include "systemuser.h"
 
 
@@ -18,11 +22,22 @@ private:
     Ui::MainWindow *ui;
     SystemUser *_user;
 
+    void closeEvent(QCloseEvent *event);
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
     void setUser(SystemUser *user);
+private slots:
+    void on_addUserBtn_clicked();
+    void on_addRackBtn_clicked();
+    void on_addProductBtn_clicked();
+    void on_addTask_clicked();
+    void on_usersTable_doubleClicked(const QModelIndex &index);
+    void on_racksTable_doubleClicked(const QModelIndex &index);
+    void on_productsTable_doubleClicked(const QModelIndex &index);
+    void on_tasksTable_doubleClicked(const QModelIndex &index);
 };
 
 #endif // MAINWINDOW_H
